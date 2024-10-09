73180c5cd0585c332c56b57e9cbbfab19093021c
pberton <1789738+pberton@users.noreply.github.com>
2024-10-08 21:58:50 -0700

    update readme (#2)

--- v1/configuration.go
+++ v1/configuration.go
@@ -92,7 +92,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "OpenAPI-Generator/0.0.1/go",
+		UserAgent:        "OpenAPI-Generator/go",
 		Debug:            false,
 		Servers:          ServerConfigurations{
 			{
